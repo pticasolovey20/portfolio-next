a2ef70d8b69fcecdc5a8f00caea94610c0ff23fa
unknown <dy.yev.solovyov@gmail.com>
2023-10-28 19:04:24 +0300

    initial commit

---
+++ pages/api/test.js
@@ -0,0 +1,5 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+const handler = (request, response) => response.status(200).json({ status: 'ok' });
+
+export default handler;
